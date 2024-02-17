7c43021d52f1429df852e0d095b268b0f3f71296
Isabella F Ortiz <141460524+CafehDoce@users.noreply.github.com>
2024-02-16 22:07:54 -0300

    Add files via upload

---
+++ untitled/src/Product.java
@@ -0,0 +1,20 @@
+public class Product {
+    public String name;
+    public String description;
+    public String price;
+    public Double weight;
+    public String nameuser;
+    public String email;
+    public Integer idade;
+    boolean desconto;
+    public void setName(String productName){
+        name = productName;
+    }
+    public String getName (){
+        return name;
+    }
+   double precoBruto;
+   double taxa;
+   double precoTotal;
+
+}
